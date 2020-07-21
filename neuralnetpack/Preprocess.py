5c6dbf34562c00a574b7f94800866305e1735725
Sam Feng <sammy@Sams-MacBook-Pro.local>
2020-07-20 22:28:28 -0700

    add data preprocessing feature

---
+++ neuralnetpack/Preprocess.py
@@ -0,0 +1,23 @@
+import numpy as np
+
+class Preprocessing:
+    def __init__(self, dataset):
+        self.dataset = dataset
+    def precess(self):
+        import numpy as np
+        import matplotlib.pyplot as plt
+        import pandas as pd
+
+        dataset = pd.read_csv(self.dataset)
+        X = dataset.iloc[:, :-1].values
+        y = dataset.iloc[:, -1].values
+
+
+        from sklearn.model_selection import train_test_split
+        X_train, X_test, y_train, y_test = train_test_split(X, y, test_size = 0.25, random_state = 0)
+
+        from sklearn.preprocessing import StandardScaler
+        sc = StandardScaler()
+        X_train = sc.fit_transform(X_train)
+        X_test = sc.transform(X_test)
+        print('Data Preprocessing Is Done!')
---
+++ neuralnetpack/SK_decision.py
@@ -0,0 +1,11 @@
+from .Preprocess import Preprocessing
+import sklearn
+
+
+def SK_decision(self):
+    def __init__(self, x_set, y_set):
+        self.x = x_set
+        self.y = y_set
+
+    def generate_skdt(self):
+        pass
